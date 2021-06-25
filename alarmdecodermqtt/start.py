146099e0d926031cc00a496add3a80c6e61684f0
KRKeegan <kevin@krkeegan.com>
2021-06-25 11:13:34 -0700

    Try to Debug Why Messages Not Appearing

--- alarmdecodermqtt/start.py
+++ alarmdecodermqtt/start.py
@@ -1,12 +1,17 @@
 import time
 import json
+import os.path
 from alarmdecoder import AlarmDecoder
 from alarmdecoder.devices import SocketDevice
 
 # Load Config
-f = open('/data/options.json')
-config = json.load(f)
-f.close()
+if os.path.isfile('/data/options.json'):
+    f = open('/data/options.json')
+    config = json.load(f)
+    f.close()
+else:
+    # Allow for local testing
+    config = {"alarm_addr": "alarmdecoder", "alarm_port": 10000}
 
 # Configuration values
 HOSTNAME = config['alarm_addr']
@@ -24,8 +29,10 @@ def main():
         # Set up an event handler and open the device
         device.on_message += handle_message
         with device.open():
+            print("Starting AlarmDecoder.")
             while True:
                 time.sleep(1)
+                print("Sleeping")
 
     except Exception as ex:
         print('Exception:', ex)
