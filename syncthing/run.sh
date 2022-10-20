5a91bfa9032ea1f161187a82efabc66fa1045429
Kevin Robert Keegan <kevin@krkeegan.com>
2022-10-20 15:11:55 -0700

    Define HOME

--- syncthing/run.sh
+++ syncthing/run.sh
@@ -7,5 +7,6 @@ if [ ! -f '/config/syncthing/config.xml' ]; then
     syncthing -generate=/config/syncthing
     sed -i 's|<address>127.0.0.1:8384</address>|<address>:8384</address>' /config/syncthing/config.xml
 fi
+export HOME=/config/syncthing/
 /usr/sbin/nginx -c /etc/nginx/nginx.conf &
-syncthing -no-browser -no-upgrade -home=/config/syncthing/
+syncthing --no-browser --no-upgrade --home=/config/syncthing/
